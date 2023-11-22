b449c9e09dc9db0848753603e406e5cddbe2dcb2
alexi <alexis_barros@outlook.pt>
2023-11-22 16:42:20 +0100

    add github actions workflow

--- src/test/java/com/houarizegai/calculator/CalculatorUITest.java
+++ src/test/java/com/houarizegai/calculator/CalculatorUITest.java
@@ -16,10 +16,17 @@ class CalculatorUITest {
         calculatorUI = new CalculatorUI();
     }
 
-    // Test the calculation method
+    // Test the calculation method\
     @ParameterizedTest
     @CsvSource({"4,6,+,10"}) //sums 4 + 6 and expects 10 {firstNumber, secondNumber, operator, expectedResult}
     void testCalculation2(double firstNumber, double secondNumber, char operator, double expectedResult) {
         assertEquals(expectedResult, calculatorUI.calculate(firstNumber, secondNumber, operator));
     }
+
+    @ParameterizedTest
+    @CsvSource({"10000000,999999999,+,1009999999"})
+    void testCalculation3(double firstNumber, double secondNumber, char operator, double expectedResult) {
+        assertEquals(expectedResult, calculatorUI.calculate(firstNumber, secondNumber, operator));
+    }
+
 }
